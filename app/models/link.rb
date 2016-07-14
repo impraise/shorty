8bdb1e84a130e3cbe0d20df66893fba0ebaff124
Dmitry Zuev <mail@dzuev.ru>
2016-07-15 02:00:31 +0300

    Write Link model validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,8 @@
 class Link < ApplicationRecord
+  validates :url, presence: true
+  validates :shortcode, presence: true, uniqueness: true,
+                        format: {
+                          with: /\A[0-9a-zA-Z_]{6}\z/,
+                          message: 'Allow only alpha-numeric and underscore'
+                        }
 end
