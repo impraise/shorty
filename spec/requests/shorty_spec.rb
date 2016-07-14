e93c6746166980cfde629016cbc3dc9b5605f84f
Dmitry Zuev <mail@dzuev.ru>
2016-07-15 01:15:22 +0300

    Finish request specs

--- spec/requests/shorty_spec.rb
+++ spec/requests/shorty_spec.rb
@@ -55,12 +55,56 @@ RSpec.describe 'Shorty', type: :request do
   end
 
   describe 'GET /:shortcode' do
-    it 'returns 404 response if the shortcode cannot be found in the system'
-    it 'returns 302 response with the location header pointing to the shortened URL'
+    it 'returns 404 response if the shortcode cannot be found in the system' do
+      get '/aaaaaa',
+          headers: { 'ACCEPT' => 'application/json' },
+          as: :json
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(404)
+    end
+
+    it 'returns 302 response with the location header pointing to the shortened URL' do
+      link = FactoryGirl.create(:link)
+      get "/#{link.shortcode}",
+          headers: { 'ACCEPT' => 'application/json' },
+          as: :json
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(link.url)
+    end
   end
 
   describe 'GET /:shortcode/stats' do
-    it 'returns 404 response if the shortcode cannot be found in the system'
-    it 'returns 200 response with stats'
+    it 'returns 404 response if the shortcode cannot be found in the system' do
+      get '/aaaaaa/stats',
+          headers: { 'ACCEPT' => 'application/json' },
+          as: :json
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(404)
+    end
+
+    it 'returns 200 response with stats for link without redirects' do
+      link = FactoryGirl.create(:link)
+      get "/#{link.shortcode}/stats",
+          headers: { 'ACCEPT' => 'application/json' },
+          as: :json
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(200)
+      expect(json['startDate']).to eq(link.created_at)
+      expect(json['lastSeenDate']).not_to be
+      expect(json['redirectCount']).to eq(0)
+    end
+
+    it 'returns 200 response with stats for link with redirects' do
+      link = FactoryGirl.create(:link, :with_redirects)
+      get "/#{link.shortcode}/stats",
+          headers: { 'ACCEPT' => 'application/json' },
+          as: :json
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(200)
+      expect(json['startDate']).to eq(link.created_at)
+      expect(json['lastSeenDate']).to eq(link.updated_at)
+      expect(json['redirectCount']).to eq(link.redirects)
+    end
   end
 end
