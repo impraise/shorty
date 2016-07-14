f1f151c473791bb80cfc37c6c0cd7813a36f513f
Dmitry Zuev <mail@dzuev.ru>
2016-07-15 01:47:25 +0300

    Add Link factory

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :link do
-    url "MyString"
-    shortcode "MyString"
-    redirects 1
+    url { Faker::Internet.url }
+    shortcode { Faker::Lorem.characters(6) }
+    created_at { Faker::Time.between(Time.zone.now - 5.days, Time.zone.now - 1.day) }
+    updated_at { created_at }
+
+    trait :with_redirects do
+      redirects { rand(10..50) }
+      updated_at { Time.zone.now }
+    end
   end
 end
