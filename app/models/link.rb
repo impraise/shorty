5f96e47539fe0af620ea89ec57a0473e34344d83
Leonid Batizhevsky <l.batizhevsky@fun-box.ru>
2016-07-19 23:00:38 +0400

    add ganeration of a short code to link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,15 @@ class Link < ApplicationRecord
   validates :url, presence: true
   validates :code, presence: true
   validates :code, uniqueness: true
+
+  before_validation :generate_code, on: :create, unless: :code
+
+  private
+
+  def generate_code
+
+    begin
+      self.code = GenerateShortCode.new.call
+    end while self.class.exists?(code: code)
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:url) { 'http://example.com' }
+  let(:code) { nil }
+
+  subject { described_class.create!(url: url, code: code) }
+
+  describe '#code' do
+    it 'presents on create' do
+      expect(subject.code).to be_present
+    end
+
+    it 'matches the criteria' do
+      criteria = /\A[0-9a-zA-Z_]{6}\z/
+      expect(subject.code).to match(criteria)
+    end
+
+    it 'is uniq' do
+      current_code = subject.code
+
+      result = described_class.new(url: 'http://another.com', code: current_code)
+      expect(result.id).to be_nil
+    end
+
+    context 'if a code assigned' do
+      let(:code) { 'my_code' }
+
+      it 'eq to the passed code' do
+        expect(subject.code).to eq 'my_code'
+      end
+    end
+  end
 end
