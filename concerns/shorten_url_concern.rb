ee7e24d926a2ee7ff5e73ae552bd6ff69308e8f0
Leonardo Mateo <leonardomateo@gmail.com>
2016-07-23 03:06:05 -0300

    Add the concern for URL shortening and storing

---
+++ concerns/shorten_url_concern.rb
@@ -0,0 +1,19 @@
+module Concerns
+  class ShortenURL
+    def initialize(attributes:, context:)
+      @ctx   = context
+      @attrs = attributes
+    end
+
+    def execute
+      puts @attrs
+      saved = @ctx.redis.call "SET", @attrs[:shortcode], @attrs[:url]
+      
+      if saved
+        @ctx.created!({ shortcode: @attrs[:shortcode] })
+      else
+        @ctx.server_error!("URL couldn't be shortened. Please try again later")
+      end
+    end
+  end
+end
