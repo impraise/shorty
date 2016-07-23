17bf5783e03a9df99a27bc2436743f8d81be8985
Leonardo Mateo <leonardomateo@gmail.com>
2016-07-23 03:06:35 -0300

    Add POST /shorten endpoint

--- routes/shortyservice.rb
+++ routes/shortyservice.rb
@@ -4,6 +4,18 @@ module Routes
       on get, root do
         format_json({ message: "Hello World" })
       end
+
+      on post do
+        on "shorten" do
+          params = parse_request
+
+          validator = Validators::ShortenURL.new(params)
+
+          error_response!(validator.errors) unless validator.valid?
+
+          Concerns::ShortenURL.new(attributes: validator.attributes, context: self).execute
+        end
+      end
     end
   end
 end
