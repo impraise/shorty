5547de4e7fc7a3b741a648df7ca0a91665d1f731
Leonardo Mateo <leonardomateo@gmail.com>
2016-07-23 02:00:34 -0300

    Add request helper methods

--- helpers/routes_helper.rb
+++ helpers/routes_helper.rb
@@ -5,6 +5,26 @@ module ShortyService
     def format_json(resource)
       res.headers["Content-Type"] = "application/json; charset=UTF-8"
       res.write JSON.dump(resource)
+
+      res.finish
     end
+
+    def json_request?
+      !!(req.env["CONTENT_TYPE"] =~ /application\/json/)
+    end
+
+    def unprocessable!(message: "Unprocessable Entity", description: "The submitted entity is unprocessable")
+
+      res.status = 422
+
+      format_json({ message: message, description: description })
+    end
+
+    def parse_request
+      unprocessable!(description: "Wrong Request Content Type") unless json_request?
+
+      JSON.parse(req.body.read)
+    end
+
   end
 end
