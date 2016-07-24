31e2a449ee56508af1a08b42e7718be28cb20b79
Evandro Dutra <evandro.eld@gmail.com>
2016-07-24 14:08:11 -0300

    creating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/shorten',         to: 'shorten_url#create'
+  get '/:shortcode',       to: 'shorten_url#show',  constraints: { shortcode: /[0-9a-zA-Z_]{4,}/ }
+  get '/:shortcode/stats', to: 'shorten_url#stats', constraints: { shortcode: /[0-9a-zA-Z_]{4,}/ }
 end
