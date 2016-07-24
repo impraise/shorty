89beacca825428fcd11bba5e5faab241a718a3bb
Leonardo Mateo <leonardomateo@gmail.com>
2016-07-23 21:00:35 -0300

    Add test for shorten URL concern

---
+++ test/concerns/shorten_url_concern_test.rb
@@ -0,0 +1,26 @@
+require_relative "../test_helper"
+
+describe "Validators::ShortenURL" do
+  before do
+    @valid_attributes = {
+      url: "http://threefunkymonkeys.com",
+      shortcode: SecureRandom.hex(3)
+    }
+
+    @context = Routes::Base.new
+  end
+
+  it "Should respond created if shortened" do
+    @context.redis.expects(:call).with("SET", @valid_attributes[:shortcode], @valid_attributes[:url]).returns(true)
+    @context.expects(:created!).with({ shortcode: @valid_attributes[:shortcode] })
+    
+    Concerns::ShortenURL.new(attributes: @valid_attributes, context: @context).execute
+  end
+
+  it "Should respond with error if not shortened" do
+    @context.redis.expects(:call).with("SET", @valid_attributes[:shortcode], @valid_attributes[:url]).returns(false)
+    @context.expects(:server_error!)
+
+    Concerns::ShortenURL.new(attributes: @valid_attributes, context: @context).execute
+  end
+end
