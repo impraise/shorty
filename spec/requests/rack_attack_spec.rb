c2cdb25ffdbe317ea816475b63045566b7d7d896
Evandro Dutra <evandro.eld@gmail.com>
2016-07-24 20:04:09 -0300

    adding API usage reference

--- spec/requests/rack_attack_spec.rb
+++ spec/requests/rack_attack_spec.rb
@@ -6,9 +6,9 @@ describe 'Rack:Attack', type: :request do
   let!(:url_address) { create(:url_address, shortcode: 'superman') }
   let(:limit) { 100 }
 
-  context "GET /:shortcode" do
+  context 'GET /:shortcode' do
     it 'must have the same status requesting below the limit' do
-      Timecop.freeze(Time.now - 15.minutes) do
+      Timecop.freeze(Time.current - 15.minutes) do
         limit.times do
           get '/superman', params: {}, headers: { 'REMOTE_ADDR' => '1.2.3.4' }
         end
@@ -18,7 +18,7 @@ describe 'Rack:Attack', type: :request do
     end
 
     it 'must change the status requesting above the limit' do
-      Timecop.freeze(Time.now - 30.minutes) do
+      Timecop.freeze(Time.current - 30.minutes) do
         (limit + 20).times do
           get '/superman', params: {}, headers: { 'REMOTE_ADDR' => '1.2.3.4' }
         end
