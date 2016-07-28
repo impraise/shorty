2a15aecba37df5101fd0b57f8dfef222d98869d1
Loren Hale <loren.hale@gmail.com>
2016-07-28 09:59:44 -0700

    add request specs

--- spec/requests/shorts_spec.rb
+++ spec/requests/shorts_spec.rb
@@ -1,10 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Shorts", type: :request do
-  describe "GET /shorts" do
-    it "works! (now write some real specs)" do
-      get shorts_path
-      expect(response).to have_http_status(200)
+
+  describe "POST /shorten" do
+    it "creates a short and returns a shortcode" do
+      post "/shorten", {url: "http://example.com"}, {}
+      parsed_json = JSON.parse(response.body)
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:created)
+      expect(parsed_json["shortcode"]).to be_truthy
     end
   end
+
+  describe "GET /:shortcode" do
+    it "redirects to the short url" do
+      short = Short.create(url: "http://example.com")
+      get "/#{short.shortcode}"
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(short.url)
+    end
+  end
+
+  describe "GET /:shortcode/stats" do
+    it "returns the short stats" do
+      short = Short.create(url: "http://example.com")
+      get "/#{short.shortcode}/stats"
+      parsed_json = JSON.parse(response.body)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_json.keys).to include("startDate", "lastSeenDate", "redirectCount")
+    end
+  end
+
 end
