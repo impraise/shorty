17c03919a434d54276a9f3dbc835c7779710e589
Loren Hale <loren.hale@gmail.com>
2016-07-28 10:04:09 -0700

    add routing spec

--- spec/routing/shorts_routing_spec.rb
+++ spec/routing/shorts_routing_spec.rb
@@ -2,38 +2,16 @@ require "rails_helper"
 
 RSpec.describe ShortsController, type: :routing do
   describe "routing" do
-
-    it "routes to #index" do
-      expect(:get => "/shorts").to route_to("shorts#index")
-    end
-
-    it "routes to #new" do
-      expect(:get => "/shorts/new").to route_to("shorts#new")
+    it "routes to #create" do
+      expect(post: "/shorten").to route_to("shorts#create")
     end
 
     it "routes to #show" do
-      expect(:get => "/shorts/1").to route_to("shorts#show", :id => "1")
+      expect(get: "/shortcode").to route_to("shorts#show", shortcode: "shortcode")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/shorts/1/edit").to route_to("shorts#edit", :id => "1")
+    it "routes to #stats" do
+      expect(get: "/shortcode/stats").to route_to("shorts#stats", shortcode: "shortcode")
     end
-
-    it "routes to #create" do
-      expect(:post => "/shorts").to route_to("shorts#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/shorts/1").to route_to("shorts#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/shorts/1").to route_to("shorts#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/shorts/1").to route_to("shorts#destroy", :id => "1")
-    end
-
   end
 end
