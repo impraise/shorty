991a8800bc097a9effeb2e841da7db210a4f98a6
hartator <hartator@gmail.com>
2016-11-07 19:41:40 -0600

    Add routes for API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/shorten', to: 'url#shorten'
+  get '/:shortcode/stats', to: 'url#stats'
+  get '/:shortcode', to: 'url#redirect'
 end
