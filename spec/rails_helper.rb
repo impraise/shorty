8329b43d05bcfa92e03da01eb6cd53a2b4d28153
hartator <hartator@gmail.com>
2016-11-07 19:41:23 -0600

    Make RSpec better at handling JSON API

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|file| require file}
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -42,4 +43,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include ApiHelper, type: :api
+  config.include Requests::JsonHelpers, type: :api
 end
