93e8e3e36b0a7ce7fd4d834070fa66d3d87e9311
Nirav Bhatia <bnirav23@gmail.com>
2017-04-01 22:12:34 +0200

    API route for /:shortcode + Tests

---
+++ routes/shortcode.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var router = express.Router();
+var UrlService = require('../services/url');
+
+router.get('/:shortcode', function(req, res) {
+
+  UrlService.get(req.params.shortcode).then(function(item) {
+    if (item) {
+      UrlService.updateStats(item).then(function(item) {
+        res.redirect(item.url);
+      });
+    }
+    else{
+      res.status(404).end();
+    }
+  });
+
+});
+
+router.get('/:shortcode/stats', function(req, res) {
+
+  UrlService.get(req.params.shortcode).then(function(item) {
+    if (item) {
+      res.json({
+        startDate: item.startDate,
+        lastSeenDate: item.lastSeenDate,
+        redirectCount: item.redirectCount
+      });
+    }
+    else{
+      res.status(404).end();
+    }
+  });
+
+});
+
+module.exports = router;
---
+++ test/routes/shortcode.js
@@ -0,0 +1,116 @@
+var chai = require('chai'), chaiHttp = require('chai-http');
+var app = require('../../index');
+var UrlService = require('../../services/url');
+
+var expect = chai.expect, assert = chai.assert;
+
+chai.use(chaiHttp);
+
+var mongoose = require('mongoose');
+var db = mongoose.createConnection(process.env.TEST_DB_URL || 'mongodb://localhost/test');
+
+describe('url shortening api', function() {
+
+  describe('getting a short url', function() {
+
+    it('should return not found if shortcode does not exist', function(done) {      
+
+      chai.request(app)
+      .get('/abcd')
+      .end(function(err, res) {
+        expect(res).to.have.status(404);
+        done();                               
+      });
+
+    });
+
+    it('should redirect if shortcode does exist', function(done) {  
+
+      UrlService.create('https://www.apple.com', 'abcdef').then(function(item) {
+        if (item) {
+          chai.request(app)
+          .get('/abcdef')
+          .end(function(err, res) {
+            expect(res).to.redirect;
+            done();                               
+          });
+        }
+      });    
+
+    });
+
+  });
+
+  describe('getting the stats for a short url', function() {
+
+    before(function() {
+      db.dropDatabase();
+    });
+
+    after(function() {
+      db.dropDatabase();
+    });
+
+    it('should return not found if shortcode does not exist', function(done) {      
+
+      chai.request(app)
+      .get('/abcd/stats')
+      .end(function(err, res) {
+        expect(res).to.have.status(404);
+        done();                               
+      });
+
+    });
+
+    it('should return the stats if shortcode does exist', function(done) {
+
+      UrlService.create('https://www.apple.com', 'abcdef').then(function(item) {
+        if (item) {
+          chai.request(app)
+          .get('/abcdef/stats')
+          .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res.body.startDate).to.exist;
+            expect(res.body.redirectCount).to.equal(0);
+            done();                               
+          });
+        }
+      });
+
+    });
+
+  })
+
+  describe('updating the stats for a short url', function() {
+
+    before(function() {
+      db.dropDatabase();
+    });
+
+    after(function() {
+      db.dropDatabase();
+    });
+
+    it('should update the redirect count on each redirect', function(done) {      
+
+      UrlService.create('https://www.apple.com', 'abcdef').then(function(item) {
+
+        expect(item.redirectCount).to.equal(0);
+
+        if (item) {
+          chai.request(app)
+          .get('/abcdef')
+          .end(function(err, res) {
+            expect(res).to.have.status(200);
+            UrlService.get('abcdef').then(function(item) {
+              expect(item.redirectCount).to.equal(1);
+              done();
+            })                               
+          });
+        }
+      });
+
+    });
+  });
+
+});
