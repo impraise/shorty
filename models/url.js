5178d06e7b8e20ee35c954579a8da6eeee47479f
Nirav Bhatia <bnirav23@gmail.com>
2017-04-01 19:52:03 +0200

    Auto generate shortcode (if none exists) before saving a Url + Require url attribute to be present

--- models/url.js
+++ models/url.js
@@ -1,13 +1,22 @@
 var mongoose = require('mongoose');
+var shortcodeLib = require('../lib/shortcode');
+
 var Schema = mongoose.Schema;
 
 var urlSchema = new Schema({
-  url: String,
+  url: {type: String, required: true},
   shortcode: String,
   lastSeenDate: Date,
   redirectCount: {type: Number, default: 0}
 }, {
   timestamps: { createdAt: 'startDate' }
+});
+
+urlSchema.pre('save', function(next) {
+  if (!this.shortcode) {
+    this.shortcode = shortcodeLib.generate();
+  }
+  next();
 });
 
 module.exports = mongoose.model('Url', urlSchema);
