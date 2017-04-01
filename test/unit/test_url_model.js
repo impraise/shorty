c019226f4389d0fd02179b994c09b8719b229315
Nirav Bhatia <bnirav23@gmail.com>
2017-04-01 19:52:44 +0200

    Tests for Url model

---
+++ test/unit/test_url_model.js
@@ -0,0 +1,67 @@
+var chai = require('chai');
+var Url = require('../../models/url');
+
+var expect = chai.expect, assert = chai.assert;
+
+var mongoose = require('mongoose');
+var db = mongoose.createConnection(process.env.TEST_DB_URL || 'mongodb://localhost/test');
+
+describe('URL model', function() {
+
+  before(function() {
+    db.dropDatabase();
+  });
+
+  after(function() {
+    db.dropDatabase();
+  });
+
+  describe('on create', function() {
+
+    it('has the redirect count set to 0', function(done) {
+
+      new Url({url: 'https://www.apple.com', shortcode: 'ABCDEF'})
+      .save()
+      .then(function(urlCreated) {
+        expect(urlCreated.redirectCount).to.equal(0);
+        done();
+      });
+
+    });
+
+    it('generates a random shortcode if none is provided', function(done) {
+
+      new Url({url: 'https://www.apple.com'})
+      .save()
+      .then(function(urlCreated) {
+        expect(urlCreated.shortcode).to.exist;
+        done();
+      });
+
+    });
+
+    it('has the startDate set', function(done) {
+
+      new Url({url: 'https://www.apple.com'})
+      .save()
+      .then(function(urlCreated) {
+        expect(urlCreated.startDate).to.exist;
+        done();
+      });
+
+    });
+
+    it('does not save without a url', function(done) {
+
+      new Url({})
+      .save()
+      .catch(function(err) {
+        expect(err.errors.url.kind).to.equal('required');
+        done();
+      });
+
+    });
+
+  });
+
+});
