d67f02ed621cd4af7413688bb5c87f761ad53d55
Victor Shcherbakov <me@v1x.info>
2017-04-25 19:41:26 +0300

    Add stub for in-memory-storage

---
+++ app/storages/in_memory_storage.rb
@@ -0,0 +1,18 @@
+# = InMemoryStorage
+class InMemoryStorage
+  def initialize
+    @records = {}
+  end
+
+  def find(shortcode)
+  end
+
+  def store(shortcode, link)
+  end
+
+  def use(shortcode)
+  end
+
+  def exists?(shortcode)
+  end
+end
