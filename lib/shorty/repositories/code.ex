b543b47b1f9109b31f9c4ca93a13cc45ee22f894
Rafael Albuquerque <me@rafael.pt>
2017-04-27 15:30:41 +0100

    Add `Code` repository
    This repository will handle all the interactions with the `Code` model.
    Takes care of the insertion, update and fetches from the database.

---
+++ lib/shorty/repositories/code.ex
@@ -0,0 +1,25 @@
+defmodule Shorty.Repositories.Code do
+  alias Shorty.Code
+
+  def find_by_id(id) do
+    Code |> Shorty.Repo.get(id)
+  end
+
+  def find_by_shortcode(shortcode) do
+    Code |> Shorty.Repo.get_by(shortcode: shortcode)
+  end
+
+  def update!(code, fields) do
+    code
+    |> Ecto.Changeset.change(fields)
+    |> Shorty.Repo.update!
+  end
+
+  def update_hit!(code) do
+    code |> update!(%{hits: code.hits + 1})
+  end
+
+  def save!(code) do
+    code |> Shorty.Repo.insert!
+  end
+end
