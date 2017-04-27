4010bd3269ba47c8cd6e7ec0d49fcce367854edf
Rafael Albuquerque <me@rafael.pt>
2017-04-27 19:16:36 +0100

    Change `UpdateCodeHits` to be fail safe

--- lib/shorty/interactors/update_code_hits.ex
+++ lib/shorty/interactors/update_code_hits.ex
@@ -1,9 +1,11 @@
 defmodule Shorty.Interactors.UpdateCodeHits do
   alias Shorty.Repositories.Code, as: CodeRepo
+  alias Shorty.Code
 
   def call(%{shortcode: shortcode}) do
-    code = shortcode |> CodeRepo.find_by_shortcode |> CodeRepo.update_hit!
-
-    {:ok, code}
+    case shortcode |> CodeRepo.find_by_shortcode do
+      code = %Code{} -> {:ok, code |> CodeRepo.update_hit!}
+      _              -> {:error, "Does not exist"}
+    end
   end
 end
