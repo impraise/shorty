cbeac283020856d1ea3202ae57525d2e34b127a4
Rafael Albuquerque <me@rafael.pt>
2017-04-28 13:16:21 +0100

    Improve /shorten endpoint controller
    Make decent use of pattern-matching.

--- web/controllers/shortcode_controller.ex
+++ web/controllers/shortcode_controller.ex
@@ -6,27 +6,18 @@ defmodule Shorty.ShortcodeController do
 
   def index(conn, %{"shortcode" => shortcode}) do
     case UpdateCodeHits.call(%{shortcode: shortcode}) do
-      {:ok, code} ->
-        conn
-        |> put_resp_header("location", code.url)
-        |> resp(:found, "")
-        |> halt
-      {:error, _} ->
-        conn
-        |> resp(:not_found, "")
-        |> halt
+      {:ok, code} -> redirect_to(conn, code.url)
+      {:error, _} -> not_found(conn)
     end
   end
 
   def stats(conn, %{"shortcode" => shortcode}) do
     case GetCode.call(%{shortcode: shortcode}) do
-      {:ok, code} ->
-        render conn, "stats.json", code
-
-      {:error, _} ->
-        conn
-        |> resp(:not_found, "")
-        |> halt
-      end
+      {:ok, code} -> render conn, "stats.json", code
+      {:error, _} -> not_found(conn)
     end
+  end
+
+  defp redirect_to(conn, url), do: conn |> put_resp_header("location", url) |> resp(:found, "") |> halt
+  defp not_found(conn), do: conn |> resp(:not_found, "") |> halt
 end
