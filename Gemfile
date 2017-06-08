0e03e37f0da0f7e439f4e56dfeb50a4dc1a77b80
Iago Moreira <iago.rn@gmail.com>
2017-06-07 23:01:52 -0300

    Add SQLite database

--- Gemfile
+++ Gemfile
@@ -2,6 +2,10 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 
+gem 'sinatra-activerecord'
+gem 'sqlite3'
+gem 'rake'
+
 group 'test' do
   gem 'rack-test'
   gem 'rspec'
--- application.rb
+++ application.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+set :database, {adapter: "sqlite3", database: "shorty.sqlite3"}
+
 
 require_relative 'lib/init'
 require_relative 'app/controllers/init'
