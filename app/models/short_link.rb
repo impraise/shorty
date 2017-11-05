9b7b7bf1d8037ed8578db78e560b4812f00dca42
Bogdan POPA <bpopa@pentalog.fr>
2017-11-05 23:25:33 +0200

    Short code generation and validations

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -2,4 +2,13 @@ class ShortLink < ApplicationRecord
   has_one :stat, dependent: :destroy
 
   validates :url, presence: true
+  validates :shortcode, format: { with: /\A[0-9a-zA-Z_]{6}\z/i }
+
+  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_".split(//)
+
+  def generate_shortcode
+    begin
+      slug = (0...6).map { ALPHABET[rand(ALPHABET.length)] }.join
+    end while ShortLink.find_by_shortcode(slug).present?
+  end
 end
