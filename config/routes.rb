236050dcca1e71afc13ae96343f2c7c83986918d
Bogdan POPA <bpopa@pentalog.fr>
2017-11-05 14:22:17 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :short_links
+  get '/:shortcode' => 'short_links#get_short_code'
+  resources :stats
 end
