8d20035c8d3912b03b4cced59f0c1a4613961dbd
Bogdan POPA <bpopa@pentalog.fr>
2017-11-05 23:26:15 +0200

    Shorten endpoint points to create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :short_links
+      post 'shorten' => 'short_links#create'
       resources :stats
       get 'get_short_code/:shortcode' => 'short_links#get_short_code'
     end
