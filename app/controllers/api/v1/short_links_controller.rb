588034e1aee8462e1f0eb53aebeb6cc23fa658be
Bogdan POPA <bpopa@pentalog.fr>
2017-11-06 11:00:30 +0200

    Create and jbuilder view

--- app/controllers/api/v1/short_links_controller.rb
+++ app/controllers/api/v1/short_links_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::ShortLinksController < ApplicationController
 
   def create
+    @short_link = ShortLink.new(short_link_params)
+    if @short_link.save
+      render status: :created
+    end
   end
 
   def get_short_code
     @short_link = ShortLink.find_by_shortcode(params[:shortcode])
   end
 
-  def index
-    @short_links = ShortLink.all
-  end
-
   private
 
   def short_link_params
-    params.permit(:url, :shortcode)
+    params.require(:short_link).permit(:url, :shortcode)
   end
 end
---
+++ app/views/api/v1/short_links/create.json.jbuilder
@@ -0,0 +1,2 @@
+json.url @short_link.url
+json.shortcode @short_link.shortcode
