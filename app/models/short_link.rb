345d75cd97b244626e0c2385cb9fb2ad28c2649a
Bogdan POPA <bpopa@pentalog.fr>
2017-11-06 12:46:38 +0200

    No more need for early return

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -7,21 +7,16 @@ class ShortLink < ApplicationRecord
 
   ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_".split(//)
 
-  before_validation { generate_shortcode(:shortcode) }
+  before_create { generate_shortcode(:shortcode) }
 
   def generate_shortcode(shortcode)
-    return if valid_preferential_shortcode?(shortcode)
     begin
       self[shortcode] = (0...6).map { ALPHABET[rand(ALPHABET.length)] }.join
     end while shortcode_exists?(shortcode)
   end
 
-  def valid_preferential_shortcode?(shortcode)
-    !(shortcode !~ /\A[0-9a-zA-Z_]{6}\z/i) && !shortcode_exists?(shortcode)
-  end
-
   def shortcode_exists?(shortcode)
-    ShortLink.exists?(shortcode => self[shortcode])
+    ShortLink.exists?(shortcode: self[shortcode])
   end
 
 end
