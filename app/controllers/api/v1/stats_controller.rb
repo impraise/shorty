0197d6b1b56d95533589a079ec613b3da8f16fb7
Bogdan POPA <bpopa@pentalog.fr>
2017-11-08 00:48:22 +0200

    Fetch stats for shortcode

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::StatsController < ApplicationController
+  before_action :find_stats, only: [:fetch_stats]
+
+  def fetch_stats
+    relevant_attrs = select_attrs(@stats)
+    json_response(relevant_attrs, :ok)
+  end
+
+  private
+
+  def select_attrs(stats)
+    stats.attributes.select do
+      |k, v| ['start_date', 'redirect_count', 'last_seen_date'].include?(k)
+    end
+  end
+
+  def find_stats
+    @short_link = ShortLink.find_by_shortcode(params[:shortcode])
+    @stats = @short_link.stat
+  end
 end
---
+++ app/views/api/v1/stats/fetch_stats.json.jbuilder
@@ -0,0 +1,4 @@
+json.ignore_nil!
+json.start_date @object['start_date']
+json.redirect_count @object['redirect_count']
+json.last_seen_date @object['last_seen_date']
