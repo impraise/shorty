5b18ac02013d4394efcecf36abef381af41a0e15
Bogdan POPA <bpopa@pentalog.fr>
2017-11-09 12:17:46 +0200

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :short_links
+      resources :short_links, only: [:create]
       get '/:shortcode/stats' => 'stats#fetch_stats'
       post 'shorten' => 'short_links#create'
       get 'fetch_short_code/:shortcode' => 'short_links#fetch_short_code'
