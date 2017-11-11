355d78f15f1900a4d55a7f23ea02e593fbf92ee7
kanmani <kanmaniselvan.m@in.reportbee.com>
2017-11-11 12:39:00 +0530

    Add url and shortcode validations before create in shorten action
    * Add validations to check for the presence of url, valid shortcode and unique shortcode before save. if conditions unmet, it response json with valid status and error message. 
    * While checking for valid shortcode, use the user input shortcode regex. Also, for generate valid shortcode use save shortcode regex.

--- app/lib/shorty.rb
+++ app/lib/shorty.rb
@@ -4,7 +4,8 @@ require 'redis'
 class Shorty
   attr_reader :url, :shortcode
 
-  SHORTCODE_REGEX = /^[0-9a-zA-Z_]{6}$/
+  SHORTCODE_POST_REGEX = /^[0-9a-zA-Z_]{4,}$/
+  SHORTCODE_SAVE_REGEX = /^[0-9a-zA-Z_]{6}$/
 
   def initialize(attributes)
     @url = attributes[:url]
@@ -33,16 +34,14 @@ class Shorty
     Shorty.new(symbolize_hash_keys(JSON.parse(shorty_record)))
   end
 
+  def has_valid_shortcode?
+    self.shortcode.to_s.match(SHORTCODE_POST_REGEX)
+  end
+
+  private
   # Establish Redis connection
   def self.redis_conn
     @redis_conn ||= Redis.new
-  end
-
-  # Convert all the keys of the hash to symbols.
-  def self.symbolize_hash_keys(hash)
-    hash.each_with_object({}) do |(k, v), temp_hash|
-      temp_hash[k.to_s.to_sym] = v
-    end
   end
 
   # Returns uniq shortcode which matches ^[0-9a-zA-Z_]{6}$ pattern
@@ -51,15 +50,18 @@ class Shorty
 
     # It's possible for the shortcode to have '-' character or the shortcode might already exist.
     # On such cases, do the regeneration.
-    if shortcode.match(SHORTCODE_REGEX).nil? || Shorty.find(shortcode)
+    if shortcode.match(SHORTCODE_SAVE_REGEX).nil? || Shorty.find(shortcode)
       return self.generate_shortcode
     end
 
     shortcode
   end
 
-  def has_valid_shortcode?
-    self.shortcode.to_s.match(SHORTCODE_REGEX)
+  # Convert all the keys of the hash to symbols.
+  def self.symbolize_hash_keys(hash)
+    hash.each_with_object({}) do |(k, v), temp_hash|
+      temp_hash[k.to_s.to_sym] = v
+    end
   end
 end
 
--- app/service.rb
+++ app/service.rb
@@ -1,19 +1,42 @@
 require 'sinatra'
 require 'pry'
-require_relative '../app/lib/shorty'
+
+require_relative 'lib/shorty'
 
 before do
   content_type :json
+end
+
+helpers do
+  def send_json_response(status, json)
+    status status
+    json.to_json
+  end
 end
 
 post '/shorten' do
   url = params[:url]
   shortcode = params[:shortcode]
 
+  # Handles if `url` param is present and it just has empty strings and quotes.
+  # Also, handles if the `url` param is not even present.
+  if url.to_s.gsub(/[\'\" ]/, '').length.zero?
+    return send_json_response(400, message: '`url` is not present')
+  end
+
+  # Return short code already present error message. If the given short code is already in use.
+  if Shorty.find(shortcode)
+    return send_json_response(409, message: 'The desired shortcode is already in use. Shortcodes are case-sensitive.')
+  end
+
+  # Checks if the given short follows the specified regex rules.
+  if !shortcode.nil? && !Shorty.new(shortcode: shortcode).has_valid_shortcode?
+    return send_json_response(422, message: 'The shortcode fails to meet the following regexp: `^[0-9a-zA-Z_]{4,}$`')
+  end
+
   shorty = Shorty.new(url: url, shortcode: shortcode).save
 
-  status 201
-  {shortcode: shorty.shortcode}.to_json
+  send_json_response(201, shortcode: shorty.shortcode)
 end
 
 get '/:shortcode' do
